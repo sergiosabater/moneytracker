f937bba0e6c098a69cc8f2117bbba620f6e330d2
Sabater Muñoz Sergio <sergiosabater@gmail.com>
2025-12-05 16:31:39 +0100

    The initial balance starts at zero

--- lib/controller/transactions_provider.dart
+++ lib/controller/transactions_provider.dart
@@ -2,33 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:moneytracker/model/transaction.dart';
 
 class TransactionsProvider extends ChangeNotifier {
-  final List<Transaction> _transactions = [
-    Transaction(
-      type: TransactionType.income,
-      amount: 1000.00,
-      description: 'Salary',
-    ),
-    Transaction(
-      type: TransactionType.expense,
-      amount: -500.00,
-      description: 'Rent',
-    ),
-  ];
+  final List<Transaction> _transactions = [];
 
   List<Transaction> get transactions => _transactions;
 
   double getTotalIncomes() {
     return _transactions
-        .where((transaction) =>  transaction.type == TransactionType.income)
+        .where((transaction) => transaction.type == TransactionType.income)
         .map((transaction) => transaction.amount)
-        .fold(0, (a,b) => a + b);
+        .fold(0, (a, b) => a + b);
   }
 
   double getTotalExpenses() {
     return _transactions
         .where((transaction) => transaction.type == TransactionType.expense)
         .map((transaction) => transaction.amount)
-        .fold(0, (a,b) => a + b);
+        .fold(0, (a, b) => a + b);
   }
 
   double getBalance() {
